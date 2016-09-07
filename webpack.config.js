90cb57fea80a0017ad6c4e99fa8d301cff2cb9b0
lukeandersen <luke@lawebdesign.com.au>
2016-09-07 10:58:11 +1000

    Simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,26 @@ const cssNext = require('postcss-cssnext');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: [
-		'./app/index.js'
-	],
+	entry: ['./app/index.js'],
 	output: {
 		path: path.join(__dirname, '/dist'),
 		filename: 'index_bundle.js'
 	},
 	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel',
-				query: {
-					presets: [
-						'react',
-						'es2015'
-					]
-				}
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader!postcss-loader'
-			},
-			{
-				test: /\.(png|jpg)$/,
-				loader: 'url-loader?limit=8192'
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel',
+			query: {
+				presets: ['react', 'es2015']
 			}
-		],
-		noParse: [/moment.js/]
+		}, {
+			test: /\.css$/,
+			loader: 'style-loader!css-loader!postcss-loader'
+		}, {
+			test: /\.(png|jpg)$/,
+			loader: 'url-loader?limit=8192'
+		}]
 	},
 	postcss() {
 		return [
