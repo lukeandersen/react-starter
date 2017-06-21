ab1c17d2617a32093f68b319b6bd71493279ae5a
lukeandersen <luke@lawebdesign.com.au>
2017-06-21 16:49:30 +1000

    Fix css nesting issue and update packages

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => (
-    <div className="container">
+    <header className="container navbar">
         <h1>brand.</h1>
         <nav className="nav">
             <Link to="/">Home</Link>
             <Link to="about">About</Link>
         </nav>
-    </div>
+    </header>
 )
 
 export default Navbar
